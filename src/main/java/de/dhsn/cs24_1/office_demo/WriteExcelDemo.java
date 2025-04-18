68bdba1540b6d3b5826a593b1dcf86944e1c8b01
AimPizza <rickomo.us@gmail.com>
2025-04-18 19:30:54 +0200

    first working test, yayy

---
+++ src/main/java/de/dhsn/cs24_1/office_demo/WriteExcelDemo.java
@@ -0,0 +1,39 @@
+package de.dhsn.cs24_1.office_demo;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class WriteExcelDemo {
+
+	public static void main(String[] args) {
+		writeSomething();
+	}
+
+	public static void writeSomething() {
+		// create workbook
+		XSSFWorkbook workbook = new XSSFWorkbook();
+
+		XSSFSheet sheet = workbook.createSheet("Testdaten");
+
+		Row row = sheet.createRow(0);
+		Cell cell = row.createCell(0);
+		cell.setCellValue("Hello from my test!");
+
+		try {
+			FileOutputStream out = new FileOutputStream(new File("test.xlsx"));
+			workbook.write(out);
+			out.close();
+			workbook.close();
+			System.out.println("successfully written to file :)");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
