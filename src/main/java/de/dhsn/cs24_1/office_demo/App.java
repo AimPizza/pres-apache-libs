74ed05f177c51b7ee6b0ccf63fd30552c8020791
AimPizza <rickomo.us@gmail.com>
2025-04-18 19:05:11 +0200

    another init

---
+++ src/main/java/de/dhsn/cs24_1/office_demo/App.java
@@ -0,0 +1,13 @@
+package de.dhsn.cs24_1.office_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
