9da302f5ae931bdfb3ba6ed37b3d4e55dba0ed77
AimPizza <rickomo.us@gmail.com>
2025-04-27 20:29:08 +0200

    parse bullet points

--- src/main/java/de/dhsn/cs24_1/office_demo/ParseWordTemplate.java
+++ src/main/java/de/dhsn/cs24_1/office_demo/ParseWordTemplate.java
@@ -1,20 +1,38 @@
 package de.dhsn.cs24_1.office_demo;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
+/* Here we try to parse the Word template specified in @see de.dhsn.cs24_1.office_demo.WordTemplate
+ * 
+ */
 public class ParseWordTemplate {
 
-	public static void main(String[] args) {
+	// TODO: should we handle this IOException?
+	public static void main(String[] args) throws IOException {
 		System.out.println("helloou");
 		// TODO: import the stuff from WordTemplate
 
 		Path msWordPath = Paths.get(WordTemplate.output);
 
 		XWPFDocument document = new XWPFDocument(Files.newInputStream(msWordPath));
+		document = new XWPFDocument(Files.newInputStream(msWordPath));
+
+		List<XWPFParagraph> paragraphs = document.getParagraphs();
+		document.close();
+
+		List<XWPFParagraph> bulletParagraphs = paragraphs.stream().filter(p -> p.getNumID() != null)
+				.collect(Collectors.toList());
+		for (XWPFParagraph paragraph : bulletParagraphs) {
+			System.out.println("• " + paragraph.getText());
+		}
 		System.out.println("ehehe I left off here");
 	}
 
