995723a92f238163be9ad5e9247ca63a98dd91e9
AimPizza <rickomo.us@gmail.com>
2025-04-30 20:14:04 +0200

    start work on writing template

--- src/main/java/de/dhsn/cs24_1/office_demo/poi/WordTemplate.java
+++ src/main/java/de/dhsn/cs24_1/office_demo/poi/WordTemplate.java
@@ -1,6 +1,8 @@
 package de.dhsn.cs24_1.office_demo.poi;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
@@ -15,29 +17,36 @@ public class WordTemplate extends XWPFDocument {
 	public static final String tasksHeading = "Tasks";
 
 	public static String output = "look-at-this.docx"; // TODO: rename into template or something
+	private XWPFDocument doc;
 
-	public WordTemplate() {
-		super();
+	public WordTemplate() throws IOException {
+		// 1) Load the .docx that already has Heading1 defined
+		try (FileInputStream in = new FileInputStream("poi/base.docx")) {
+			this.doc = new XWPFDocument(in);
+		} // TODO
+
+		// 2) Now any .setStyle("Heading1") will pick up the real Heading1 style
 		createBaseTemplate();
 	}
 
 	public void createBaseTemplate() {
 		// Elements come in Paragraphs
-		XWPFParagraph title = createParagraph();
-		title.setAlignment(ParagraphAlignment.CENTER);
+		XWPFParagraph title = doc.createParagraph();
+		title.setStyle("Heading1");
+		title.setAlignment(ParagraphAlignment.LEFT);
 
 		// Content of an element comes in a Run
 		XWPFRun titleRun = title.createRun(); // region of text with common set of properties
-		titleRun.setText("I changed this - Amogus");
+		titleRun.setText(documentHeading);
 		titleRun.setColor("000000");
 		titleRun.setBold(true);
-		titleRun.setFontFamily("Courier");
+		titleRun.setFontFamily("Helvetica");
 		titleRun.setFontSize(20);
 	}
 
 	public void save() {
 		try (FileOutputStream out = new FileOutputStream(output)) {
-			this.write(out);
+			doc.write(out);
 			System.out.println("great success!");
 		} catch (Exception e) {
 			System.out.println("error saving");
@@ -45,7 +54,7 @@ public class WordTemplate extends XWPFDocument {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("halo");
 
 		WordTemplate document = new WordTemplate();
