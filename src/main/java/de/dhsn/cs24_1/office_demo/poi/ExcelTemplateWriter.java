f104b6e784c41ae533d829059cb71535b89cc5d7
AimPizza <rickomo.us@gmail.com>
2025-05-07 20:32:08 +0200

    ExcelTemplate: add column resizing and table filters

--- src/main/java/de/dhsn/cs24_1/office_demo/poi/ExcelTemplateWriter.java
+++ src/main/java/de/dhsn/cs24_1/office_demo/poi/ExcelTemplateWriter.java
@@ -2,7 +2,10 @@ package de.dhsn.cs24_1.office_demo.poi;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.AreaReference;
+import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFTable;
 
 public class ExcelTemplateWriter {
 
@@ -15,12 +18,28 @@ public class ExcelTemplateWriter {
 		ExcelDemoWriter document = new ExcelDemoWriter(new String[] { "Aufgaben" });
 		XSSFSheet sheet = document.workbook.getSheetAt(0);
 
+		AreaReference reference = document.workbook.getCreationHelper().createAreaReference(new CellReference(1, 1),
+				new CellReference(5, 5));
+		XSSFTable table = sheet.createTable(reference);
+		// I didn't fix the IDs like a user on stackoverflow suggests doing
+
+		table.setName("Aufgabentabelle");
+		table.setDisplayName("Aufgabentabelle (Displayname?)");
+
+		// filter
+		table.getCTTable().addNewAutoFilter().setRef(table.getArea().formatAsString());
+
 		Row headerRow = sheet.createRow(1);
 		for (int i = 0; i < headerElements.length; i++) {
 			String headerElement = headerElements[i];
+			Integer columnIndex = i + 1;
 
-			Cell cell = headerRow.createCell(i + 1);
+			Cell cell = headerRow.createCell(columnIndex);
 			cell.setCellValue(headerElement);
+
+			sheet.autoSizeColumn(columnIndex);
+			// make some extra space
+			sheet.setColumnWidth(columnIndex, sheet.getColumnWidth(columnIndex) + 5 * 256);
 		}
 
 		document.save("./template.xlsx");
