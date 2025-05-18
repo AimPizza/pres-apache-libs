7176dde005aaff1edfb2b4617c67bec8a8c4c94d
Linda <linda.gecaj@outlook.com>
2025-05-18 21:10:02 +0200

    add example for pdf encrypting

---
+++ src/main/java/de/dhsn/cs24_1/office_demo/pdf/PDFEncrypting.java
@@ -0,0 +1,40 @@
+package de.dhsn.cs24_1.office_demo.pdf;
+
+import static de.dhsn.cs24_1.office_demo.shared.Utilities.log;
+
+import java.io.File;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
+import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
+
+public class PDFEncrypting {
+
+	public static void main(String args[]) throws Exception {
+		// Loading an existing document
+		File file = new File("pdf/test.pdf");
+		PDDocument document = PDDocument.load(file);
+
+		// Creating access permission object
+		AccessPermission ap = new AccessPermission();
+
+		// Creating StandardProtectionPolicy object
+		StandardProtectionPolicy spp = new StandardProtectionPolicy("test", "test", ap);
+
+		// Setting the length of the encryption key
+		spp.setEncryptionKeyLength(128);
+
+		// Setting the access permissions
+		spp.setPermissions(ap);
+
+		// Protecting the document
+		document.protect(spp);
+
+		log("Document encrypted. :0");
+
+		// Saving the document
+		document.save("pdf/test_pw.pdf");
+		// Closing the document
+		document.close();
+	}
+}
